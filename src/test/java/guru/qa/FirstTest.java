2cb353994c8983347109cb8325049a58a197d8fa
Aleksey Plechkov <email@email.ru>
2022-05-25 00:40:21 +0300

    refactoring

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -7,11 +7,12 @@ public class FirstTest {
 
     @Test
     void firstTest(){
-        Assertions.assertTrue(3>2);
+
+        Assertions.assertFalse(3<2);
     }
 
     @Test
     void secondTest(){
-        Assertions.assertTrue(3>1);
+        Assertions.assertFalse(3<1);
     }
 }
